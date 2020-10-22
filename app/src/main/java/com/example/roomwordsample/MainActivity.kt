b00376d9f66d94651784b8ae7d9745adce6d179d
Denis Potapov <fantom.r45@mail.ru>
2020-10-22 23:54:39 +0500

    final commit

---
+++ app/src/main/java/com/example/roomwordsample/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.roomwordsample
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val newWordActivityRequestCode = 1
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var wordViewModel: WordViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val wordAdapter = WordListAdapter(this@MainActivity)
+        recyclerview.adapter = wordAdapter
+        recyclerview.layoutManager = LinearLayoutManager(this@MainActivity)
+
+        wordViewModel = ViewModelProvider(this).get(WordViewModel::class.java)
+        wordViewModel.allWords.observe(this, Observer { words ->
+            // Update the cached copy of the words in the adapter.
+            words?.let { wordAdapter.setWords(it) }
+        })
+
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, NewWordActivity::class.java)
+            startActivityForResult(intent, newWordActivityRequestCode)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == newWordActivityRequestCode && resultCode == Activity.RESULT_OK) {
+            data?.getStringExtra(NewWordActivity.EXTRA_REPLY)?.let {
+                val word = Word(it)
+                wordViewModel.insert(word)
+            }
+        } else {
+            Toast.makeText(applicationContext, R.string.empty_not_saved, Toast.LENGTH_LONG).show()
+        }
+    }
+}
+
